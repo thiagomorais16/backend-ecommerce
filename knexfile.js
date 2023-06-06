5e0cf61dc5ff83c95bee13b944fe2eb24e2ddf7f
root <root@DESKTOP-J6NBS78>
2023-06-06 18:04:15 -0300

    all update

--- knexfile.js
+++ knexfile.js
@@ -6,34 +6,34 @@
 module.exports = {
 
   development: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host: '127.0.0.1',
-      port: 3307,
+      port: 3306,
       user: 'thiago',
-      password: 'thiago',
+      password: 'thiago123',
       database: 'back_ecommerce'
     }
   },
 
   staging: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host: '127.0.0.1',
-      port: 3307,
+      port: 3306,
       user: 'thiago',
-      password: 'thiago',
+      password: 'thiago123',
       database: 'back_ecommerce'
     }
   },
 
   production: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host: '127.0.0.1',
-      port: 3307,
+      port: 3306,
       user: 'thiago',
-      password: 'thiago',
+      password: 'thiago123',
       database: 'back_ecommerce'
     }
   }
--- migrations/20230605012910_criar_usuarios_table.js
+++ migrations/20230605012910_criar_usuarios_table.js
@@ -2,14 +2,16 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('usuarios', (table) => {
+        table.bigIncrements('id').primary();
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTable('usuarios')
 };
